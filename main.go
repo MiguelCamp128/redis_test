bb7f8eadaa88d15f622a4815e6921a7506f77adb
Miguel <miguel.campano@protonmail.com>
2021-09-03 13:42:19 +0200

    ip

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 	client := redis.NewClient(&redis.Options{
 		Addr: net.JoinHostPort(redisHost, "6379"),
 		TLSConfig: &tls.Config{
-			ServerName:   "redis-server",
+			ServerName:   "10.10.50.116",
 			Certificates: []tls.Certificate{cert},
 			RootCAs:      pool,
 		},
