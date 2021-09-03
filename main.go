38de18e227b7be2a71a90eaddba8a4e46f5abe13
Miguel <miguel.campano@protonmail.com>
2021-09-03 12:01:07 +0200

    First test
    Primera prueba

--- main.go
+++ main.go
@@ -5,32 +5,62 @@ import (
 	// la v8
 	//NO RECOMENDABLE
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
 
 func main() {
-	ctx := context.Background()
- 
-	rdb := redis.NewClient(&redis.Options{
-		Addr:	  "10.10.50.116:6379",
-		Password: "redis-password", // no password set
-		DB:		  0,  // use default DB
-	})
+		
+	redisHost := os.Getenv("localhost") // e.g. "1.2.3.4", "127.0.0.1", "localhost", "redis.acmecorp.com"
 
-	err := rdb.Set(ctx, "key", "cambio", 0).Err()
+	cert, err := tls.LoadX509KeyPair("./tls/redis.crt", "./tls/redis.key")
 	if err != nil {
 		panic(err)
 	}
 
-	val, err := rdb.Get(ctx, "prueba").Result()
+	caCert, err := ioutil.ReadFile("./tls/CA.crt")
+	if err != nil {
+		panic(err)
+	}
+	pool := x509.NewCertPool()
+	pool.AppendCertsFromPEM(caCert)
+
+	ctx := context.Background()
+
+	client := redis.NewClient(&redis.Options{
+		Addr: net.JoinHostPort(redisHost, "6379"),
+		TLSConfig: &tls.Config{
+			ServerName:   "redis-server",
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      pool,
+		},
+		Password: "redis-password", 
+	})
+
+	duration := time.Second
+	
+			
+	err = client.Set(ctx, "key", "key-content", 0).Err()
+	if err != nil {
+		fmt.Println("Error en el set")
+		time.Sleep(duration*5)
+		panic(err)
+	}
+	
+	val, err := client.Get(ctx, "prueba").Result()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("key", val)
 
-	val2, err := rdb.Get(ctx, "key2").Result()
+	val2, err := client.Get(ctx, "key2").Result()
 	if err == redis.Nil {
 		fmt.Println("key2 does not exist")
 	} else if err != nil {
