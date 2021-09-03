049dcbfe23179626a67ebdc564409c8cb27740bb
Miguel <miguel.campano@protonmail.com>
2021-09-03 14:59:13 +0200

    pt3

--- main.go
+++ main.go
@@ -5,74 +5,48 @@ import (
 	// la v8
 	//NO RECOMENDABLE
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
 
 	"github.com/go-redis/redis/v8"
 )
 
 func main() {
-	var ctx = context.Background()
-
-
-    rdb := redis.NewClient(&redis.Options{
-        Addr:     "10.10.50.116:6379",
-        Password: "redis-server", // no password set
-        DB:       0,  // use default DB
-    })
-
-    err := rdb.Set(ctx, "key", "value", 0).Err()
-    if err != nil {
-        panic(err)
-    }
-
-    val, err := rdb.Get(ctx, "key").Result()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("key", val)
-
-    val2, err := rdb.Get(ctx, "key2").Result()
-    if err == redis.Nil {
-        fmt.Println("key2 does not exist")
-    } else if err != nil {
-        panic(err)
-    } else {
-        fmt.Println("key2", val2)
-    }
-    // Output: key value
-    // key2 does not exist
-}
-
 		
-	// redisHost := os.Getenv("10.10.50.116") // e.g. "1.2.3.4", "127.0.0.1", "localhost", "redis.acmecorp.com"
+	redisHost := os.Getenv("10.10.50.116") // e.g. "1.2.3.4", "127.0.0.1", "localhost", "redis.acmecorp.com"
 
-	// cert, err := tls.LoadX509KeyPair("/home/jenkins/.ssh/redis.crt", "/home/jenkins/.ssh/redis.key")
-	// if err != nil {
-	// 	log.Fatal("Certs", err)
-	// }
+	cert, err := tls.LoadX509KeyPair("/home/jenkins/.ssh/redis.crt", "/home/jenkins/.ssh/redis.key")
+	if err != nil {
+		log.Fatal("Certs", err)
+	}
 
-	// caCert, err := ioutil.ReadFile("/home/jenkins/.ssh/CA.crt")
-	// if err != nil {
-	// 	log.Fatal("CertsCA", err)
-	// }
-	// pool := x509.NewCertPool()
-	// pool.AppendCertsFromPEM(caCert)
+	caCert, err := ioutil.ReadFile("/home/jenkins/.ssh/CA.crt")
+	if err != nil {
+		log.Fatal("CertsCA", err)
+	}
+	pool := x509.NewCertPool()
+	pool.AppendCertsFromPEM(caCert)
 
-	// ctx := context.Background()
+	ctx := context.Background()
 
 
-	// client := redis.NewClient(&redis.Options{
-	// 	Addr: net.JoinHostPort(redisHost, "6379"),
-	// 	Password: "redis-password", 
-	// 	TLSConfig: &tls.Config{
-	// 		MinVersion: tls.VersionTLS12,
-	// 		//InsecureSkipVerify: true,
-	// 		ServerName:   "redis-server",
-	// 		Certificates: []tls.Certificate{cert,},
-	// 		RootCAs:      pool,
-	// 	},
+	client := redis.NewClient(&redis.Options{
+		Addr: net.JoinHostPort(redisHost, "6379"),
+		Password: "redis-password", 
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+			//InsecureSkipVerify: true,
+			ServerName:   "redis-server",
+			Certificates: []tls.Certificate{cert,},
+			RootCAs:      pool,
+		},
 		
-	// })
+	})
 //nada
 	//duration := time.Second
 	
@@ -83,20 +57,20 @@ func main() {
 	// 	log.Fatal("SET", err)
 	// }
 	
-	// val, err := client.Get(ctx, "key").Result()
-	// if err != nil {
-	// 	log.Fatal("GET", err)
-	// }
-	// fmt.Println("key", val)
+	val, err := client.Get(ctx, "key").Result()
+	if err != nil {
+		log.Fatal("GET", err)
+	}
+	fmt.Println("key", val)
 
-	// val2, err := client.Get(ctx, "key2").Result()
-	// if err == redis.Nil {
-	// 	fmt.Println("key2 does not exist")
-	// } else if err != nil {
-	// 	panic(err)
-	// } else {
-	// 	fmt.Println("key2", val2)
-	// }
+	val2, err := client.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
 	// Output: key value
 	// key2 does not exist
-//}
+}
